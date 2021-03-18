ad97a336d084a8f6be96ceedfc91d61b548067c5
sigit3329 <muhammadsigitnugroho@students.amikom.ac.id>
2021-03-18 13:54:24 +0700

    file taxi 1.0

---
+++ Tugas Taxi/Tugas_taxi/Tugas_taxi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File_taxi taxi = new File_taxi
+            {
+                Nama_Driver = "Jono",
+                OnDuty = true,
+                NumPassenger = 10
+            };
+
+            taxi.info();
+            taxi.naik();//memanggil void naik yg ada di file_taxi.cs
+            taxi.turun();
+
+            Console.ReadKey();
+        }
+    }
+}
