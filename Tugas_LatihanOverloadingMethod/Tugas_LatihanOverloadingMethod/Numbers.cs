f0ee949a3979575a4acdb5124157b09b8d798f13
sigit3329 <muhammadsigitnugroho@students.amikom.ac.id>
2021-04-01 18:38:23 +0700

    1.0
    Tugas_LatihanOverloadingMethod

---
+++ Tugas_LatihanOverloadingMethod/Tugas_LatihanOverloadingMethod/Numbers.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_LatihanOverloadingMethod
+{
+    class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
+        }
+
+        public float FindMinimum(int number1, int number2, float number3)
+        {
+            if (number1 < number2 && number1 < number3)
+            {
+                return number1;
+            }
+            else if (number2 < number1 && number2 < number3)
+            {
+                return number2;
+            }
+            else
+            {
+                return (int)number3;
+            }
+
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            if (number1 > number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
+        }
+
+        public float FindMaximum(int number1, int number2, float number3)
+        {
+            if (number1 > number2 && number1 > number3)
+            {
+                return number1;
+            }
+            else if (number2 > number1 && number2 > number3)
+            {
+                return number2;
+            }
+            else
+            {
+                return (int)number3;
+            }
+        }
+    }
+}
---
+++ Tugas_LatihanOverloadingMethod/Tugas_LatihanOverloadingMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_LatihanOverloadingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+
+            Console.WriteLine($"Minimum #1 : {numbers.FindMinimum(3, 1)}");
+            Console.WriteLine($"Minimum #2 : {numbers.FindMinimum(3, 2, 4)}");
+            Console.WriteLine();
+            Console.WriteLine($"Maximum #1 : {numbers.FindMaximum(3, 1)}");
+            Console.WriteLine($"Maximum #2 : {numbers.FindMaximum(3, 2, 4)}");
+
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
