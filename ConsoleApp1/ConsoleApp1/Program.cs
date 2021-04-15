d6536292c7a248c30eefc3e00590364f35c9676f
sigit3329 <muhammadsigitnugroho@students.amikom.ac.id>
2021-04-15 12:27:39 +0700

    git

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ResponsiPemrograman2684
+{
+    class Program
+    {
+        public class Karyawan
+        {
+            public int identitas;
+            public string nama;
+            public int gaji;
+            public Karyawan(int nik, string name, int gajibulanan)
+            {
+
+                identitas = nik;
+                nama = name;
+                gaji = gajibulanan;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NO" + " NIK/NAMA" + "         " + "Gaji Bulanan");
+            Karyawan karyawan1 = new Karyawan(19112684, "Gafi", 3000000);
+            Karyawan karyawan2 = new Karyawan(17111919, "Ghema", 2000000);
+            Console.WriteLine("1." + karyawan1.identitas + " " + karyawan1.nama + "     " + karyawan1.gaji);
+            Console.WriteLine("2." + karyawan2.identitas + " " + karyawan2.nama + "    " + karyawan2.gaji);
+            if (karyawan1.gaji <= 0 || karyawan2.gaji <= 0)
+            {
+                Console.WriteLine("Anda memasukkan gaji minus");
+                karyawan1.gaji = 0;
+                karyawan2.gaji = 0;
+                Console.WriteLine("1." + karyawan1.identitas + " " + karyawan1.nama + "     " + karyawan1.gaji);
+                Console.WriteLine("2." + karyawan2.identitas + " " + karyawan2.nama + "    " + karyawan2.gaji);
+            }
+            else
+            {
+                Console.WriteLine("Asyiiiiik kenaikan gaji 10%!!");
+                int tambah = karyawan1.gaji * 10 / 100;
+                karyawan1.gaji += tambah;
+                int tambah1 = karyawan2.gaji * 10 / 100;
+                karyawan2.gaji += tambah1;
+                Console.WriteLine("1." + karyawan1.identitas + " " + karyawan1.nama + "     " + karyawan1.gaji);
+                Console.WriteLine("2." + karyawan2.identitas + " " + karyawan2.nama + "    " + karyawan2.gaji);
+            }
+
+        }
+    }
+}
---
+++ ResponsiPemrograman3329/ResponsiPemrograman3329/karyawan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2836
+{
+    class karyawan
+    {
+
+        public string NIK { get; set; }
+        public string Nama { get; set; }
+        public int gajiBulanan { get; set; }
+
+        public karyawan(int no, string nik, string nama, int gajibulanan)
+        {
+
+            this.NIK = nik;
+            this.Nama = nama;
+            if (gajibulanan < 0)
+            {
+                this.gajiBulanan = 0;
+            }
+            else
+            {
+                this.gajiBulanan = gajibulanan;
+            }
+        }
+
+
+    }
+}
